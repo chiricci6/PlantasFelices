e0db1adefadeb11f6fc8f4e12cc5ee17aaf44cef
chiricci6 <chiricci6@gmail.com>
2023-10-09 11:26:25 -0300

    el seguidor

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function mostrarTexto(elemento) {
-    elemento.classList.add('mostrar-texto');
-  }
-  
+const imagenSeguidora = document.getElementById('imagenSeguidora');
+
+document.addEventListener('mousemove', (event) => {
+  const mouseX = event.clientX + window.pageXOffset; // Coordenadas X del mouse con desplazamiento de página
+  const mouseY = event.clientY + window.pageYOffset; // Coordenadas Y del mouse con desplazamiento de página
+
+  // Mueve la imagen al cursor sin restricciones
+  imagenSeguidora.style.left = mouseX + 'px';
+  imagenSeguidora.style.top = mouseY + 'px';
+});
